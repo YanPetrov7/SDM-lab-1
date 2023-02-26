37fc406b947694b7a62e614585472c102b7706a5
YanPetrov7 <yanemerald2004@gmail.com>
2023-02-26 14:16:29 +0200

    fix repeated code in interactive mode

--- interactive.js
+++ interactive.js
@@ -1,28 +1,18 @@
 const readlineSync = require('readline-sync');
 
-const questions = {
-  a: 'a:',
-  b: 'b:',
-  c: 'c:',
-};
-
-const question = (text) => {
+const askUser = (text) => {
   let value = readlineSync.question(text);
   if (isNaN(Number(value)) === true || value === '') {
     console.error(`Error. Expected a valid real number, got '${value}' instead`);
-    value = question(text);
+    value = askUser(text);
   }
   return value;
 };
 
 const interStart = () => {
-  const firstValue = question(questions.a);
-  const secondValue = question(questions.b);
-  const thirdValue = question(questions.c);
-
-  const arr = [firstValue, secondValue, thirdValue];
-
-  return arr;
+  const questions = ['a: ','b: ','c: '];
+  const answers = questions.map(elem => askUser(elem));
+  return answers;
 };
 
 module.exports = interStart;
